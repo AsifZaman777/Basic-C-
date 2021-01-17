e0428d7f01d422d49f7a626393de28896dc1b42c
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-01-17 21:53:10 +0600

    Add files via upload

---
+++ arithmatic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b, value;
+    cout<<"a =";
+    cin>>a;
+    cout<<"b =";
+    cin>>b;
+    value=a*b;
+
+    cout<<"\t\tvalue :"<< value<<endl;
+    return 0;
+}
